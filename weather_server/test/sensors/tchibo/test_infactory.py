729984c51702c290d5e16f939a67bb196c18e224
Christian Heger <7235795+BadIdeaException@users.noreply.github.com>
2026-05-30 16:09:28 +0200

    test: complete partial test for decoder happy path

--- weather_server/test/sensors/tchibo/test_infactory.py
+++ weather_server/test/sensors/tchibo/test_infactory.py
@@ -2,24 +2,30 @@ import pytest
 from sensors.tchibo.infactory import InFactory
 
 class TestInFactory:
-	@pytest.fixture
-	def input(self):
-		return '0000111100110000010111001110011101100001'
-				
-	def test_throws_on_wrong_length(self, input):
-		decoder = InFactory()
-		input = ''
+    @pytest.fixture
+    def input(self):
+        return '0000111100110000010111001110011101100001'
+                
+    def test_throws_on_wrong_length(self, input):
+        decoder = InFactory()
+        input = ''
 
-		with pytest.raises(ValueError):
-			decoder.decode(input)
+        with pytest.raises(ValueError):
+            decoder.decode(input)
 
-	def test_throws_on_crc_fail(self, input):
-		decoder = InFactory()
-		input = input[:7] + '0000' + input[13:] # Overwrite CRC
+    def test_throws_on_crc_fail(self, input):
+        decoder = InFactory()
+        input = input[:7] + '0000' + input[13:] # Overwrite CRC
 
-		with pytest.raises(ValueError):
-			decoder.decode(input)
+        with pytest.raises(ValueError):
+            decoder.decode(input)
 
-	def test_decodes_values(self, input):
-		decoder = InFactory()		
-		values = decoder.decode(input)
+    def test_decodes_values(self, input):
+        decoder = InFactory()       
+        values = decoder.decode(input)
+
+        assert values['device_id'] == 0x0F
+        assert values['battery_low'] == False
+        assert values['temperature'] == pytest.approx(14.777777)
+        assert values['humidity'] == 76
+        assert values['channel'] == 1
