c2e9ae9edee530cc7d2dead255b7cf83bdabe7ba
Christian Heger <7235795+BadIdeaException@users.noreply.github.com>
2026-06-02 18:48:09 +0200

    test: adjust tests to BME280 forced mode fix

--- weather_server/test/sensors/builtin/test_builtin.py
+++ weather_server/test/sensors/builtin/test_builtin.py
@@ -11,6 +11,7 @@ class TestBuiltin:
         bme280.temperature = mocker.sentinel.temperature
         bme280.pressure = mocker.sentinel.pressure
         bme280.humidity = mocker.sentinel.humidity        
+        bme280.measuring = False
 
         with Builtin(1.0, bme280) as builtin:
             reading = await asyncio.wait_for(anext(builtin.readings()), 0.1)
@@ -24,6 +25,7 @@ class TestBuiltin:
     async def test_yields_according_to_interval(self, mocker):
         INTERVAL = 0.2
         bme280 = mocker.MagicMock(spec=BME280)
+        bme280.measuring = False
         with Builtin(INTERVAL, bme280) as builtin:
             gen = builtin.readings()
             await asyncio.wait_for(anext(gen), INTERVAL * 2)
@@ -37,6 +39,7 @@ class TestBuiltin:
     @pytest.mark.asyncio
     async def test_close_terminates_generator(self, mocker):
         bme280 = mocker.MagicMock(spec=BME280)
+        bme280.measuring = False
         with Builtin(1.0, bme280) as builtin:
             gen = builtin.readings()
 
