82eaefef8305f24c24d2d5af26250b66a6e7c8e6
Christian Heger <7235795+BadIdeaException@users.noreply.github.com>
2026-06-02 18:20:28 +0200

    fix: reading never changing
    Previously, the sensor set BME280 to forced mode on creation. This made the
    BME280 take a single reading, but did not cause it to update on subsequent
    reads from the data register - the sensed values would never change.
    
    In fact, BME280 mode needs to be set to forced ANY time a reading is to be
    taken.
    
    This commit corrects forced mode handling by the builtin sensor.

--- weather_server/src/sensors/builtin/builtin.py
+++ weather_server/src/sensors/builtin/builtin.py
@@ -4,15 +4,25 @@ import asyncio
 class Builtin:
     def __init__(self, interval, bme280: BME280 | None = None):
         if bme280 is None:
-            bme280 = BME280(1, 0x76, ttl=1.0)
-            bme280.mode = BME280.Mode.FORCED
+            bme280 = BME280(1, 0x76, ttl=1.0)            
+            bme280.temperature_oversampling = BME280.Oversampling.X1
+            bme280.pressure_oversampling    = BME280.Oversampling.X1
+            bme280.humidity_oversampling    = BME280.Oversampling.X1
 
         self.bme280 = bme280
         self.interval = interval
         self._closed = False
 
     async def readings(self):
+        def wait_for_measurement():
+            while self.bme280.measuring:
+                pass
+
         while not self._closed:
+            # Force a measurement
+            self.bme280.mode = BME280.Mode.FORCED
+            # Wait until measurement is complete
+            await asyncio.to_thread(wait_for_measurement)
             yield {
                 'temperature': self.bme280.temperature, 
                 'pressure':    self.bme280.pressure,
