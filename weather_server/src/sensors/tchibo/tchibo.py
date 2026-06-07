26ec8bb39cb759a5a049d45888342444b32c3aa2
Christian Heger <7235795+BadIdeaException@users.noreply.github.com>
2026-06-07 17:00:34 +0200

    feat: emit status on Tchibo sensor

--- weather_server/src/sensors/tchibo/tchibo.py
+++ weather_server/src/sensors/tchibo/tchibo.py
@@ -1,5 +1,7 @@
+import asyncio
+import contextlib
 from .receiver import Receiver
-from .infactory import InFactory
+from .infactory import InFactory, CRCError, PacketLengthError
 
 class Tchibo:
     def __init__(self, receiver: Receiver | None = None):
@@ -13,6 +15,8 @@ class Tchibo:
             receiver = Receiver(timings)
 
         self.receiver = receiver
+        self._status = asyncio.Queue(maxsize=100)
+
 
     async def readings(self):
         decoder = InFactory()
@@ -21,8 +25,34 @@ class Tchibo:
                 try:
                     reading = decoder.decode(packet)
                     yield reading
-                except ValueError:
-                    pass # silently ignore packets with wrong length, as they are a normal part of RF operation
+                except CRCError as e:
+                    with contextlib.suppress(asyncio.QueueFull):
+                        self._status.put_nowait(e)
+                except PacketLengthError:
+                    pass # silently ignore packets with wrong length, as they are a normal part of RF operation                
+
+
+    async def status(self):
+        async def forward_receiver_status():
+            async for event in self.receiver.status():
+                with contextlib.suppress(asyncio.QueueFull):
+                    self._status.put_nowait(event)
+        
+        task = asyncio.create_task(forward_receiver_status())
+        try:
+            while True:
+                yield await self._status.get()
+        finally:
+            task.cancel()
+
+
+    def __enter__(self):
+        return self
+
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
 
     def close(self):
         self.receiver.close()
--- weather_server/test/sensors/tchibo/test_tchibo.py
+++ weather_server/test/sensors/tchibo/test_tchibo.py
@@ -1,6 +1,8 @@
 import pytest
+import asyncio
 from sensors.tchibo import Tchibo
 from sensors.tchibo.receiver import Receiver
+from sensors.tchibo.infactory import PacketLengthError, CRCError
 
 class TestTchibo:
     @pytest.mark.asyncio
@@ -22,19 +24,57 @@ class TestTchibo:
         assert result is mocker.sentinel.readings
         
     @pytest.mark.asyncio
-    async def test_swallows_malformed(self, mocker):
+    async def test_does_not_emit_wrong_length_packets_to_status(self, mocker):
         async def packets():
-            yield mocker.sentinel.good_packet
-            yield mocker.sentinel.malformed_packet
+            yield mocker.sentinel.packet
 
         receiver = mocker.MagicMock(spec=Receiver)
         receiver.__enter__.return_value = receiver
         receiver.receive.return_value = packets()
         decoder = mocker.patch('sensors.tchibo.tchibo.InFactory').return_value
-        decoder.decode.side_effect = [ ValueError('CRC check failed'), mocker.sentinel.readings ]
+        decoder.decode.side_effect = [ PacketLengthError('') ]
+        tchibo = Tchibo(receiver)
+
+        with pytest.raises(asyncio.TimeoutError):
+            await asyncio.wait_for(anext(tchibo.status()), timeout=0.1)
+
+
+    @pytest.mark.asyncio
+    async def test_emits_crc_check_failures_to_status(self, mocker):
+        async def packets():
+            yield mocker.sentinel.packet
+
+        receiver = mocker.MagicMock(spec=Receiver)
+        receiver.__enter__.return_value = receiver
+        receiver.receive.return_value = packets()
+        decoder = mocker.patch('sensors.tchibo.tchibo.InFactory').return_value
+        decoder.decode.side_effect = [ CRCError(0, 0, '') ]
+        tchibo = Tchibo(receiver)
+        
+        task = asyncio.create_task(anext(tchibo.readings()))
+        try:
+            result = await asyncio.wait_for(anext(tchibo.status()), timeout=0.1)
+        finally:
+            task.cancel()
+
+        assert isinstance(result, CRCError)
+
+
+    @pytest.mark.asyncio
+    async def test_continues_after_malformed_packets(self, mocker):
+        async def packets():
+            yield mocker.sentinel.short_packet
+            yield mocker.sentinel.bad_crc_packet
+            yield mocker.sentinel.good_packet
+
+        receiver = mocker.MagicMock(spec=Receiver)
+        receiver.__enter__.return_value = receiver
+        receiver.receive.return_value = packets()
+        decoder = mocker.patch('sensors.tchibo.tchibo.InFactory').return_value
+        decoder.decode.side_effect = [ CRCError(0, 0, ''), PacketLengthError(''), mocker.sentinel.readings ]
         tchibo = Tchibo(receiver)
 
         result = await anext(tchibo.readings())
 
-        assert decoder.decode.call_count == 2
+        assert decoder.decode.call_count == 3
         assert result is mocker.sentinel.readings
